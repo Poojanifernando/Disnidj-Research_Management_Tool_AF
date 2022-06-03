b51a9a721b4cc5ef07b2fbb6ead50d3cf79cabb2
IT20026448 <it20026448@my.sliit.lk>
2022-06-03 06:36:57 +0530

    debbiecommit

---
+++ Backend/models/chatUser.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const {isEmail} = require('validator');
+const bCrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema ({
+    name : {
+        type : String,
+        required : [true, "Can't be blank"]
+    },
+    email: {
+        type : String,
+        lowercase : true,
+        unique : true,
+        required : [true, "Can't be blank"],
+        index : true,
+        validate : [isEmail, "invalid email"] 
+    },
+    password : {
+        type : String,
+        required : [true, "Can't be blank"],
+    },
+    pictures : {
+        type : String,
+    },
+    newMessages : {
+        type : Object,
+        default : {}
+    },
+    status : {
+        type : String,
+        default : 'online'
+    }
+}, {minimize : false});
+
+UserSchema.pre('save', function(next) {
+    const user = this;
+    if(!user.isModified('password')) return next();
+
+    bCrypt.genSalt(10, function(err, salt){
+        if(err) return next(err);
+
+        bCrypt.hash(user.password, salt, function(err, hash){
+            if(err) return next(err);
+
+            user.password = hash
+            next();
+        })
+    }) 
+})
+
+UserSchema.methods.toJSON = function() {
+    const user = this;
+    const userObject = user.toObject();
+    delete userObject.password;
+    return userObject;
+}
+
+UserSchema.statics.findByCredentials = async function(email, password) {
+    const user = await chatUser.findOne({email}); //find user by credentials
+
+    if(!user) throw new Error('invalid email or password');
+
+    const isMatch = await bCrypt.compare(password, user.password);
+    if(!isMatch) throw new Error('invalid email or password');
+    return user; // if password entered matches, return user
+}
+
+const chatUser = mongoose.model('chatUser', UserSchema);
+
+module.exports = chatUser;
